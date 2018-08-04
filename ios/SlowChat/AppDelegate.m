5fae3379ee2174d64daeb326bdabf7966520da1f
zhaochuanwei02 <zhaochuanwei02@meituan.com>
2018-08-04 18:54:56 +0800

    代码修改

--- ios/SlowChat/AppDelegate.m
+++ ios/SlowChat/AppDelegate.m
@@ -27,7 +27,12 @@
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
+
+  UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:rootViewController];
+  navigationController.navigationBar.hidden = YES;
+  self.window.rootViewController = navigationController;
+  // 原先
+  // self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
 }
