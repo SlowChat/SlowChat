845c5e920c3c555bbad98d9a742bbb4775987b79
hulanlan656 <hulanlan656@jk.cn>
2018-08-14 12:18:08 +0800

    忘记密码修改

--- app/user/EditPassword.js
+++ app/user/EditPassword.js
@@ -12,6 +12,10 @@ import {
 import { get, post } from '../utils/request'
 import Toast from 'react-native-easy-toast'
 
+import { isMobileNumberSupport } from '../utils/util'
+import VerifyCode from '../components/VerifyCode'
+import SuccessModal from '../components/SuccessModal'
+
 const ICONS = {
   forward: require('../images/icon_forward.png'),
 }
@@ -24,9 +28,11 @@ export default class EditPassowrd extends Component {
     }
   }
   state = {
-    username: '',
+    mobile: '',
     vCode: '',
-    password: ''
+    password: '',
+    isVcodeClick: false,
+    isSucc: false   //成功提示框
   }
   componentDidMount() {
 
@@ -34,8 +40,8 @@ export default class EditPassowrd extends Component {
 
   handleSubmit = () => {
     const { navigate, pop } = this.props.navigation;
-    const { username, vCode, password } = this.state;
-    post('api/user/passwordReset.html', { username: username, verification_code: vCode, password: password }).then((res) => {
+    const { mobile, vCode, password } = this.state;
+    post('api/user/passwordReset.html', { username: mobile, verification_code: vCode, password: password }).then((res) => {
       console.log(res)
       if (res.code === 1) {
         this.refs.toast.show(res.msg);
@@ -60,6 +66,35 @@ export default class EditPassowrd extends Component {
     })
   }
 
+  handleChangeMobile = (text) => {
+    this.setState({
+      mobile: text
+    })
+    if (text.length >= 11 && isMobileNumberSupport(text)) {
+      this.setState({isVcodeClick: true})
+    } else {
+      this.setState({isVcodeClick: false})
+    }
+  }
+
+  handleMobile = () => {
+    const { mobile } = this.state;
+    if (!isMobileNumberSupport(mobile)) this.refs.toast.show('您的手机号输入有误');
+  }
+
+  inputVcode = (text) => {
+    this.setState({vCode: text})
+    if (text.length >= 6) {
+      this.setState({
+        isClick: true
+      })
+    }
+  }
+
+  onRequestClose = () => {
+    this.setState({ isSucc: false })
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -68,21 +103,23 @@ export default class EditPassowrd extends Component {
             <Text style={styles.label}>手机号</Text>
             <TextInput
               style={styles.input}
-              onChangeText={(text) => this.setState({username: text})}
+              onChangeText={(text) => this.handleChangeMobile(text)}
+              onBlur={() => this.handleMobile()}
               placeholder='请输入您的手机号'
               value={this.state.username}
             />
-            <TouchableWithoutFeedback onPress={() => this.handleVcode()}>
+            <VerifyCode mobile={this.state.mobile} isVrfy={this.state.isVcodeClick}/>
+            {/* <TouchableWithoutFeedback onPress={() => this.handleVcode()}>
               <View style={styles.btn}>
                 <Text style={styles.btnTxt}>获取验证码</Text>
               </View>
-            </TouchableWithoutFeedback>
+            </TouchableWithoutFeedback> */}
           </View>
           <View style={styles.menu}>
             <Text style={styles.label}>验证码</Text>
             <TextInput
               style={styles.input}
-              onChangeText={(text) => this.setState({vCode: text})}
+              onChangeText={(text) => this.inputVcode(text)}
               placeholder='请输入您的验证码'
               value={this.state.vCode}
             />
@@ -108,6 +145,15 @@ export default class EditPassowrd extends Component {
           </Text>
         </View>
         <Toast ref="toast" position="bottom" />
+        <SuccessModal
+          txt={'手机号修改成功'}
+          btn={'返回'}
+          visible={this.state.isSucc}
+          onPress={() => {
+            this.props.navigation.pop() // navigate
+          }}
+          onClose={this.onRequestClose}
+        />
       </View>
     );
   }
