e74fc52266aba3dcb9fc500411d1da2c5bb066f2
zhaochuanwei02 <zhaochuanwei02@meituan.com>
2018-08-25 17:23:14 +0800

    代码修改

--- app/App.js
+++ app/App.js
@@ -56,7 +56,7 @@ const StackApp = (isLogin) => createStackNavigator({
     }
   }
 }, {
-  initialRouteName: 'Setting',
+  initialRouteName: 'BottomTabs',
   // isLogin ? 'BottomTabs' : 'Login',
   navigationOptions: {
     // headerBackTitleVisible: false,
